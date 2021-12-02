8f3c368e1d959dce36c899591aa1dd544c52ec89
LFBorchert <leonardborchert@mail.de>
2021-12-02 17:19:25 +0100

    Calculation of climatologies adapted to new structure and ported to Python3

--- STEP1b_MakeClimatologies/MakeModelSSTClim.py
+++ STEP1b_MakeClimatologies/MakeModelSSTClim.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+# The above is required for sbatch submission
+
 import numpy as np
 import pickle
 import os
 import glob
 import sys
+import xarray as xr
+from scipy import interpolate
 
 analogue_var = 'SST'
-model = sys.argv[1]
+model = sys.argv[3]
 
 # The problem with using a very early clim period is that the obs are actually just THEIR clim,
 # which is 1960-1990 anyway
@@ -16,44 +21,88 @@ clim_start, clim_end = 1960, 1990
 # clim_start, clim_end = 1980, 1990
 
 myhost = os.uname()[1]
-print "myhost = {:s}".format(myhost)
+print("myhost = {:s}".format(myhost))
+usr = os.environ["USER"]
 
 if 'ciclad' in myhost:
     # Ciclad options
     datadir = '/data/mmenary/python_saves/CMIP_{:s}/'.format(analogue_var)
 else:
     # Jasmin options
-    datadir = '/work/scratch-nopw/mmenary/CMIP_{:s}/'.format(analogue_var)
+    datadir = '/work/scratch-nopw/{}/CMIP_{:s}/'.format(usr,analogue_var)
 
-climatology_file = os.path.join(datadir, 'CMIP_{:s}_{:s}_historical-EnsMn_TM{:d}-{:d}_Annual.pkl'.format(analogue_var, model, clim_start, clim_end))
+climatology_file = os.path.join(datadir, 'CMIP_{:s}_{:s}_historical-EnsMn_TM{:d}-{:d}_Annual.nc'.format(analogue_var, model, clim_start, clim_end))
 
 tolerance = 0.9 * (clim_end - clim_start)
+
+# Prepare regridding
+def regrid_sst(sst_in, year_in):
+    nyrs = len(year_in)
+    area = get_area()
+    sst_regridded = np.ma.masked_all(shape=(nyrs, nj, ni))
+
+    for tt in range(nyrs):
+#         if tt not in [97, 98, 99, 100]: continue
+        print('{:d}/{:d}'.format(tt, nyrs))
+#        print(len(np.array([lat_in.ravel(), lon_in.ravel()]).T))
+#        print(len(sst_in[0, 0, :, :].ravel()))
+#        print(len(sst_in[0, 0, :, :].mask.ravel()))
+        sst_regridded[tt, :, :] = interpolate.griddata(np.array([lat_in.ravel(), lon_in.ravel()]).T,
+                                                       sst_in[0, tt, :, :].ravel(), (lat_re, lon_re),
+                                                       method='linear')
+    mask_regridded = interpolate.griddata(np.array([lat_in.ravel(), lon_in.ravel()]).T,
+                                          sst_in[0, 0, :, :].mask.ravel(), (lat_re, lon_re),
+                                          method='linear')
+    sst_regridded = np.ma.array(sst_regridded, mask=np.repeat(mask_regridded[np.newaxis, :, :], nyrs, axis=0))
+    return sst_regridded
+
+def get_area():
+    radius_earth = 6.4e6
+    dlon = dlat = np.radians(1.)
+    area = np.ones(shape=(nj, ni))
+    for jj in range(nj):
+        area[jj, :] *= radius_earth**2 * np.cos(np.radians(lat_re[jj])) * np.sin(dlon) * np.sin(dlat)
+    return area
+
 
 if os.path.isfile(climatology_file):
     print('Already created climatology file:  {:s}'.format(climatology_file))
 else:
-    hist_files = glob.glob(os.path.join(datadir, 'CMIP?_{:s}_{:s}_historical-*_Annual_Regridded.pkl'.format(analogue_var, model)))
-    hist_files.sort()
+    hist_files_field = glob.glob(os.path.join(datadir, 'CMIP?_{:s}field_{:s}_historical-*_Annual.nc'.format(analogue_var, model)))
+    hist_files_field.sort()
+    hist_files_mask = glob.glob(os.path.join(datadir, 'CMIP?_{:s}mask_{:s}_historical-1_Annual.nc'.format(analogue_var, model)))
 
     nj, ni = 180, 360
+    lon_re = np.repeat((np.arange(-180, 180) + 0.5)[np.newaxis, :], nj, axis=0)
+    lat_re = np.repeat((np.arange(-90, 90) + 0.5)[:, None], ni, axis=1)
     climatology = np.zeros(shape=(nj, ni))
     count = 0.
-    for ifile, hist_file in enumerate(hist_files):
-        print ifile, hist_file
+    for ifile, hist_file in enumerate(hist_files_field):
+        print(ifile, hist_file)
 
-        with open(hist_file, 'rb') as handle:
-            sst_in, year_in = pickle.load(handle)
-            clim_years = np.nonzero((year_in > clim_start) * (year_in <= clim_end))[0]
-            nyrs = len(clim_years)
-            if nyrs < tolerance:
-                continue
+        ds_mask = xr.open_dataset(hist_files_mask[0]).to_array()
+        mask_in = ds_mask.values
+        ds_field = xr.open_dataset(hist_file).to_array()
+        sst_read = np.ma.masked_array(ds_field.values,mask=mask_in)
+        lon_in = ds_field['lon'].values
+        lat_in = ds_field['lat'].values
+        year_in = ds_field['time'].values
 
-            climatology += np.ma.mean(sst_in[clim_years[0]:clim_years[-1], :, :], axis=0)
-            count += 1.
+        sst_in = regrid_sst(sst_read,year_in)
+
+        clim_years = np.nonzero((year_in > clim_start) * (year_in <= clim_end))[0]
+        nyrs = len(clim_years)
+        if nyrs < tolerance:
+            continue
+
+        climatology += np.ma.mean(sst_in[clim_years[0]:clim_years[-1], :, :], axis=0)
+        count += 1.
 
     climatology /= count
     climatology = np.ma.masked_array(climatology, mask=sst_in[clim_years[0], :, :].mask)
 
-    with open(climatology_file, 'wb') as handle:
-        print('Writing to:  {:s}'.format(climatology_file))
-        pickle.dump(climatology, handle,  protocol=pickle.HIGHEST_PROTOCOL)
+    clim_print = xr.DataArray(climatology, name='climatology', dims = ['y','x'], coords = {'lat': (['y','x'],lat_re), 'lon': (['y','x'],lon_re)}).to_dataset(name='climatology')
+
+    #with open(climatology_file, 'wb') as handle:
+    print('Writing to:  {:s}'.format(climatology_file))
+    clim_print.to_netcdf(path=climatology_file,format="NETCDF4")
--- STEP1b_MakeClimatologies/SUBMIT_MakeClimatology.sh
+++ STEP1b_MakeClimatologies/SUBMIT_MakeClimatology.sh
@@ -3,8 +3,13 @@
 echo "Beginning loops!"
 
 var=SST
+cursory_initial_check="True"
 echo "Running for username: $USER"
 usr=$USER
+
+# This can be PICON (for CMIP5+6 combined), CMIP5, CMIP6, DAMIP6
+typeset -l choice  # To ignore case
+choice=$1
 
 scripts_dir=${ANALOGUE_SCRIPTS_DIR:-"UNSET"}
 if [[ $scripts_dir == "UNSET" ]]
@@ -12,7 +17,7 @@ then
   echo -e "\nANALOGUE_SCRIPTS_DIR is not set. Run setup.sh and re-source .bash_profile\n"
 fi
 
-output_dir=/work/scratch-nopw/${usr}/output1b
+output_dir=/work/scratch-nopw/${usr}/output
 datadir=/work/scratch-nopw/${usr}/CMIP_${var}
 runscript=${scripts_dir}/STEP1b_MakeClimatologies/MakeModel${var}Clim.py
 lists_dir=${scripts_dir}/model_lists
@@ -21,8 +26,80 @@ max_jobs=5000
 
 export PYTHONPATH="$scripts_dir/python_modules/:${PYTHONPATH}"
 
-# Created by doing: "sort cmip6_list.txt cmip5_list.txt damip_list.txt | uniq > all_models_list.txt"
-model_list=${lists_dir}/all_models_list.txt
+# Updated list, created by doing: ls -d */* | cut -f2 --delim='/' | uniq > ~/python/scripts/cmip5_list.txt
+if [[ $choice == 'cmip5' ]]
+then
+  model_list=${lists_dir}/cmip5_list.txt
+  experiments="historical rcp45 rcp85"
+  ens_mems="1 2 3 4 5 6 7 8 9 10"
+        projects="CMIP5"
+elif [[ $choice == 'cmip6' ]]
+then
+  model_list=${lists_dir}/cmip6_list.txt
+  experiments="historical ssp126 ssp585"
+  ens_mems="1 2 3 4 5 6 7 8 9 10"
+        projects="CMIP6"
+elif [[ $choice == 'picon' ]]
+then
+  model_list=${lists_dir}/cmip5_and_cmip6_list.txt
+  experiments="piControl"
+  ens_mems="1"
+        projects="CMIP5 CMIP6"
+elif [[ $choice == 'damip6' ]]
+then
+  model_list=${lists_dir}/damip_list.txt
+  experiments="hist-GHG hist-aer hist-nat hist-stratO3"
+  ens_mems="1 2 3 4 5 6 7 8 9 10"
+        projects="CMIP6"
+elif [[ $choice == 'test' ]]
+then
+  model_list=${lists_dir}/test_list.txt
+  experiments="historical"
+  ens_mems="1"
+  projects="CMIP5 CMIP6"
+else
+  echo "Invalid choice!"
+  exit
+fi
+
+echo "Running for ${choice}. Setting expts/ens-mems/model-list accordingly:"
+echo "MODELS: $model_list"
+echo "EXPTS: $experiments"
+echo "ENS-MEMS: $ens_mems"
+sleep 1
+
+remake_saves="True"
+testing="False"
+seasonal="False"
+time_series_only="False"
+
+if [[ $testing == "True" ]]
+then
+  test_string="TEST"
+else
+  test_string=""
+fi
+
+if [[ $seasonal == "True" ]]
+then
+  period="Seasonal"
+else
+  period="Annual"
+fi
+
+if [[ $var == "SST" ]]
+then
+        realm="Omon"
+        var_cmip="thetao"
+elif [[ $var == "SAT" ]]
+then
+        realm="Amon"
+        var_cmip="tas"
+elif [[ $var == "MSLP" ]]
+then
+        realm="Amon"
+        var_cmip="psl"
+fi
 
 requiredDirectories="$output_dir $datadir"
 for requiredDirectory in $requiredDirectories
@@ -33,17 +110,80 @@ done
 
 while read model
 do
-  echo $model
+        echo $model
+        for experiment in $experiments
+        do
+                for ens_mem in $ens_mems
+                do
+      run_this="True"
 
-  output=${output_dir}/Make${var}Clim_${model}.out
-  error=${output_dir}/Make${var}Clim_${model}.err
+                        if [[ $cursory_initial_check == "True" ]]
+                        then
+                                err1=1
+                                err2=1
+                                for project in $projects
+                                do
+                                        if [[ $project == "CMIP6" ]]
+                                        then
+                                                ls /badc/cmip6/data/CMIP6/*/*/${model}/*${experiment}/r${ens_mem}i*/${realm}/${var_cmip}/g*/latest > /dev/null 2>&1
+                                                err1=$?
+                                        elif [[ $project == "CMIP5" ]]
+                                        then
+                                                ls /badc/cmip5/data/cmip5/output1/*/${model}/${experiment}/mon/ocean/${realm}/r${ens_mem}i*/latest/${var_cmip} > /dev/null 2>&1
+                                                err2=$?
+                                        fi
+                                done
 
-  # ${scripts_dir}/queue_spacer_sbatch.sh $max_jobs  # This will check every 120s if I have less than 100 jobs in the Q
+        echo "err1 $err1 err2 $err2"
+        if [[ $err1 != 0 ]] && [[ $err2 != 0 ]]
+        then
+          echo " == No input data, skipping..."
+          echo "/badc/cmip6/data/CMIP6/*/*/${model}/*${experiment}/r${ens_mem}i*/${realm}/${var_cmip}/g*/latest"
+          echo "/badc/cmip5/data/cmip5/output1/*/${model}/${experiment}/mon/ocean/${realm}/r${ens_mem}i*/latest/${var_cmip}"
+          run_this="False"
+        fi
+                        fi
 
-  cmd="sbatch -p $queue -t 6:00:00 -n 1 -o ${output} -e ${error} ${runscript} ${model}"
-  echo $cmd
+      vars_concat="${experiment}_${ens_mem}_${model}_${period}_${time_series_only}${test_string}"
 
-  $cmd
+      # Check for previously saved data
+      if [[ $remake_saves == "False" ]]
+      then
+                                for project in $projects
+                                do
+                check_file=${datadir}/${project}_${var}_${model}_${experiment}-${ens_mem}_${period}.pkl${test_string}
+                echo "Checking: ${check_file}"
+                ls ${check_file} > /dev/null 2>&1
+                err=$?
+                if [[ $err == 0 ]]
+                                then
+                  echo "Previous save data exists - skipping"
+                                                run_this="False"
+                                fi
+                                done
+      fi
+
+                        if [[ $run_this == "False" ]]
+                        then
+                                continue
+                        fi
+
+      output=${output_dir}/${var}_CMIP_${vars_concat}.out
+      error=${output_dir}/${var}_CMIP_${vars_concat}.err
+
+                        ${scripts_dir}/queue_spacer_sbatch.sh $max_jobs  # This will check every 120s if I have less than 100 jobs in the Q
+
+                        cmd="sbatch -p $queue -t 6:00:00 -n 1 -o ${output} -e ${error} ${runscript} ${experiment} ${ens_mem} ${model} ${period} ${time_series_only} ${testing} ${lists_dir}"
+                        echo $cmd
+
+                        $cmd
+
+                        ######################################################################
+                        # exit ###############################################################
+                        ######################################################################
+
+    done
+  done
 done < $model_list
 
 echo "Submit script finished"
